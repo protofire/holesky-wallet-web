ca5febec950f9728982619dbf14e8b72e32d2f0c
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2024-02-09 11:27:19 +0700

    feat: hide unnecessary cookies

--- src/components/common/CookieBanner/index.tsx
+++ src/components/common/CookieBanner/index.tsx
@@ -10,8 +10,8 @@ import { selectCookies, CookieType, saveCookieConsent } from '@/store/cookiesSli
 import { selectCookieBanner, openCookieBanner, closeCookieBanner } from '@/store/popupSlice'
 
 import css from './styles.module.css'
-import { AppRoutes } from '@/config/routes'
-import ExternalLink from '../ExternalLink'
+// import { AppRoutes } from '@/config/routes'
+// import ExternalLink from '../ExternalLink'
 
 const COOKIE_WARNING: Record<CookieType, string> = {
   [CookieType.NECESSARY]: '',
@@ -55,8 +55,8 @@ export const CookieBanner = ({
   }
 
   const handleAcceptAll = () => {
-    setValue(CookieType.UPDATES, true)
-    setValue(CookieType.ANALYTICS, true)
+    // setValue(CookieType.UPDATES, true)
+    // setValue(CookieType.ANALYTICS, true)
     setTimeout(handleAccept, 300)
   }
 
@@ -73,7 +73,7 @@ export const CookieBanner = ({
           <Grid item xs>
             <Typography variant="body2" mb={2}>
               By clicking &quot;Accept all&quot; you agree to the use of the tools listed below and their corresponding
-              cookies. <ExternalLink href={AppRoutes.cookie}>Cookie policy</ExternalLink>
+              cookies. {/* <ExternalLink href={AppRoutes.cookie}>Cookie policy</ExternalLink> */}
             </Typography>
 
             <Grid container alignItems="center" gap={4}>
@@ -84,7 +84,7 @@ export const CookieBanner = ({
                   <Typography variant="body2">Locally stored data for core functionality</Typography>
                 </Box>
 
-                <Box mb={2}>
+                {/* <Box mb={2}>
                   <CookieCheckbox
                     checkboxProps={{ ...register(CookieType.UPDATES), id: 'beamer' }}
                     label="Beamer"
@@ -104,7 +104,7 @@ export const CookieBanner = ({
                   <Typography variant="body2">
                     Opt in for Google Analytics cookies to help us analyze app usage patterns.
                   </Typography>
-                </Box>
+                </Box> */}
               </Grid>
             </Grid>
 

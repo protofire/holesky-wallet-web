b780a98ae543f3a884b7db51443a017066b759e1
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2024-02-09 11:28:59 +0700

    feat: hide unnecessary items from the sidebar

--- src/components/sidebar/SidebarFooter/index.tsx
+++ src/components/sidebar/SidebarFooter/index.tsx
@@ -3,21 +3,21 @@ import { useCallback, useEffect } from 'react'
 
 import {
   SidebarList,
-  SidebarListItemButton,
-  SidebarListItemIcon,
-  SidebarListItemText,
+  // SidebarListItemButton,
+  // SidebarListItemIcon,
+  // SidebarListItemText,
 } from '@/components/sidebar/SidebarList'
-import { BEAMER_SELECTOR, loadBeamer } from '@/services/beamer'
+import { /*BEAMER_SELECTOR,*/ loadBeamer } from '@/services/beamer'
 import { useAppDispatch, useAppSelector } from '@/store'
 import { selectCookies, CookieType } from '@/store/cookiesSlice'
 import { openCookieBanner } from '@/store/popupSlice'
-import BeamerIcon from '@/public/images/sidebar/whats-new.svg'
-import HelpCenterIcon from '@/public/images/sidebar/help-center.svg'
+// import BeamerIcon from '@/public/images/sidebar/whats-new.svg'
+// import HelpCenterIcon from '@/public/images/sidebar/help-center.svg'
 import { ListItem } from '@mui/material'
 import DebugToggle from '../DebugToggle'
-import { HELP_CENTER_URL, IS_PRODUCTION } from '@/config/constants'
-import Track from '@/components/common/Track'
-import { OVERVIEW_EVENTS } from '@/services/analytics/events/overview'
+import { /*HELP_CENTER_URL,*/ IS_PRODUCTION } from '@/config/constants'
+// import Track from '@/components/common/Track'
+// import { OVERVIEW_EVENTS } from '@/services/analytics/events/overview'
 import { useCurrentChain } from '@/hooks/useChains'
 
 const SidebarFooter = (): ReactElement => {
@@ -48,7 +48,7 @@ const SidebarFooter = (): ReactElement => {
         </ListItem>
       )}
 
-      <Track {...OVERVIEW_EVENTS.WHATS_NEW}>
+      {/* <Track {...OVERVIEW_EVENTS.WHATS_NEW}>
         <ListItem disablePadding>
           <SidebarListItemButton id={BEAMER_SELECTOR} onClick={handleBeamer}>
             <SidebarListItemIcon color="primary">
@@ -74,7 +74,7 @@ const SidebarFooter = (): ReactElement => {
             </SidebarListItemButton>
           </a>
         </ListItem>
-      </Track>
+      </Track> */}
     </SidebarList>
   )
 }

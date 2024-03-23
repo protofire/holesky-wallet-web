cc906d59aad89161d044f6fe5938143f7a44071c
nick8319 <zhiltsov.nick@gmail.com>
2024-03-23 21:39:56 +0100

    fix: title

--- src/pages/welcome/accounts.tsx
+++ src/pages/welcome/accounts.tsx
@@ -6,7 +6,7 @@ const Accounts: NextPage = () => {
   return (
     <>
       <Head>
-        <title>{'Safe{Wallet} – My accounts'}</title>
+        <title>{'Holesky Safe – My accounts'}</title>
       </Head>
 
       <MyAccounts />

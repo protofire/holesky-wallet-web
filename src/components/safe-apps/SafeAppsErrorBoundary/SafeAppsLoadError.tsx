e8020bfc9d7d8061e1b24ae0861cd9dcaf44785a
ajimeno04 <jimenobeltran11@gmail.com>
2024-12-09 11:02:10 +0100

    feat.a dd link to protofire support

--- src/components/safe-apps/SafeAppsErrorBoundary/SafeAppsLoadError.tsx
+++ src/components/safe-apps/SafeAppsErrorBoundary/SafeAppsLoadError.tsx
@@ -21,7 +21,7 @@ const SafeAppsLoadError = ({ onBackToApps }: SafeAppsLoadErrorProps): React.Reac
 
         <div>
           <Typography component="span">In case the problem persists, please report it through our </Typography>
-          <ExternalLink href={''} fontSize="medium">
+          <ExternalLink href={'https://safe-support.protofire.io/'} fontSize="medium">
             {' '}
             Form
           </ExternalLink>

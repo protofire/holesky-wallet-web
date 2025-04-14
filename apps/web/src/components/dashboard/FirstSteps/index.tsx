b4fd38992f79c265dbd35f114c9208dc942b8bb4
ajimeno04 <jimenobeltran11@gmail.com>
2025-04-14 13:44:09 +0200

    refactor(FirstSteps): simplify dApp exploration title and content

--- apps/web/src/components/dashboard/FirstSteps/index.tsx
+++ apps/web/src/components/dashboard/FirstSteps/index.tsx
@@ -112,8 +112,8 @@ const UsefulHintsWidget = () => {
           Did you know
         </Typography>
       }
-      title="Explore over 70+ dApps"
-      content="In our Safe App section you can connect your Safe to over 70 dApps directly or via Wallet Connect to interact with any application."
+      title="Explore dApps"
+      content="In our Safe App section you can connect your Safe to dApps directly or via Wallet Connect to interact with any application."
       completed={false}
     />
   )

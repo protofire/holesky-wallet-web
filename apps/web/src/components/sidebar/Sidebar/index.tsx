08a49aeb46372bf654bc18d187bd3c5fe17dc650
ajimeno04 <jimenobeltran11@gmail.com>
2025-04-15 11:09:01 +0200

    refactor(Sidebar): comment out IndexingStatus and Divider components in sidebar

--- apps/web/src/components/sidebar/Sidebar/index.tsx
+++ apps/web/src/components/sidebar/Sidebar/index.tsx
@@ -6,7 +6,6 @@ import ChainIndicator from '@/components/common/ChainIndicator'
 import SidebarHeader from '@/components/sidebar/SidebarHeader'
 import SidebarNavigation from '@/components/sidebar/SidebarNavigation'
 import SidebarFooter from '@/components/sidebar/SidebarFooter'
-import IndexingStatus from '@/components/sidebar/IndexingStatus'
 
 import css from './styles.module.css'
 import { trackEvent, OVERVIEW_EVENTS } from '@/services/analytics'
@@ -54,9 +53,9 @@ const Sidebar = (): ReactElement => {
         {/* What's new + Need help? */}
         <SidebarFooter />
 
-        <Divider flexItem />
+        {/* <Divider flexItem />
 
-        <IndexingStatus />
+        <IndexingStatus /> */}
       </div>
       <Drawer variant="temporary" anchor="left" open={isDrawerOpen} onClose={onDrawerToggle}>
         <div className={css.drawer}>
